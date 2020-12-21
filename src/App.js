8735dcc3a4e50e79ca45e5d16e97f1f061f8abd9
Xavier Carpenter <xaviercarpenter@Xaviers-MBP.attlocal.net>
2020-12-21 00:47:43 -0600

    configured firebase hosting

--- src/App.js
+++ src/App.js
@@ -1,34 +1,109 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.scss";
 import Forecast from "./components/Forecast";
 import cloudDrizzle from "./images/Cloud-Drizzle-Alt.png";
+import _uniqBy from "lodash.uniqby";
+import moment from "moment";
+import { Switch } from "@material-ui/core";
 
-function App() {
-  return (
-    <main className="app-container">
-      <div className="location">
-        <h3 className="city">Dallas, TX</h3>
-        <p>Saturday, Sep 16, 2018</p>
-      </div>
-      <span className="date"></span>
-      <div className="forecast-box">
-        <div className="forecast-img">
-          <div className="current-forecast">
-            <h2>93&#176;</h2>
-            <img
-              src={cloudDrizzle}
-              alt="Cloud Drizzle"
-              width="44.2"
-              height="39.3"
-            ></img>
-            <p>Partly Cloudy</p>
-            <p>12 mph</p>
-          </div>
+const API_key = "302d513c633f66e83b6b0b25d89aab2f";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      forecast: [],
+      unit: "imperial",
+      unitChanged: false,
+      todaysForecast: {},
+    };
+    this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    this.getWeather();
+  }
+
+  handleChange = (event) => {
+    const isCelcius = event.target.checked;
+
+    if (isCelcius) {
+      this.setState({ unit: "metric" }, () => {
+        this.getWeather();
+      });
+    } else {
+      this.setState({ unit: "imperial" }, () => {
+        this.getWeather();
+      });
+    }
+  };
+
+  getWeather = async () => {
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/forecast?q=Dallas&units=${this.state.unit}&appid=${API_key}`
+    );
+    const response = await api_call.json();
+    // console.log("Original response ", response.list);
+    const forecast = response.list.map((obj, i) => {
+      const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+      const date = new Date(obj.dt_txt);
+      const day = days[date.getDay()];
+      return {
+        date: obj.dt_txt,
+        day,
+        temp: Math.round(obj.main.temp),
+        weather: obj.weather[0].main,
+        weatherDesc: obj.weather[0].description,
+        wind: Math.round(obj.wind.speed),
+      };
+    });
+
+    const simplifedForecast = _uniqBy(forecast, function (e) {
+      return e.day;
+    });
+
+    this.setState({ forecast: simplifedForecast });
+    this.setState({ todaysForecast: this.state.forecast[0] });
+
+    console.log("Simplified Response ", this.state.forecast);
+  };
+  render() {
+    return (
+      <main className="app-container">
+        <div className="location">
+          <h3 className="city">Dallas, TX</h3>
+          <p>{moment().format("dddd, MMM Do YYYY")}</p>
         </div>
-        <Forecast />
-      </div>
-    </main>
-  );
+        <div className="forecast-box">
+          <div className="forecast-img">
+            <div className="current-forecast">
+              <h2>{this.state.todaysForecast.temp}&#176;</h2>
+              <img
+                src={cloudDrizzle}
+                alt="Cloud Drizzle"
+                width="44.2"
+                height="39.3"
+              ></img>
+              <div className="weather-desc">
+                <p>{this.state.todaysForecast.weatherDesc}</p>
+                <p>{this.state.todaysForecast.wind} mph</p>
+              </div>
+            </div>
+            <div className="unit-switch">
+              <span>F&#176;</span>
+              <Switch
+                class="switch"
+                onChange={(e) => this.handleChange(e)}
+                name="changeUnit"
+              />
+              <span>C&#176;</span>
+            </div>
+          </div>
+          <Forecast forecast={this.state.forecast} />
+        </div>
+      </main>
+    );
+  }
 }
 
 export default App;
--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,70 +1,49 @@
 import React from "react";
 import cloudDrizzle from "../images/Cloud-Drizzle-Alt.png";
-import cloudDrizzleSun from "../images/Cloud-Drizzle-Sun-Alt.png";
+// import cloudDrizzleSun from "../images/Cloud-Drizzle-Sun-Alt.png";
 import cloudLightning from "../images/Cloud-Lightning.png";
 import cloudSun from "../images/Cloud-Sun.png";
 import "./Forecast.scss";
 
-const Forecast = () => {
+const Forecast = ({ forecast }) => {
+  const chooseWeatherIcons = (weather) => {
+    let weatherIcon;
+    switch (weather) {
+      case weather === "Clear":
+        weatherIcon = cloudSun;
+        break;
+      case weather === "Rain":
+        weatherIcon = cloudDrizzle;
+        break;
+      case weather === "Clouds":
+        weatherIcon = cloudSun;
+        break;
+      case weather === "Extreme":
+        weatherIcon = cloudLightning;
+        break;
+      default:
+        weatherIcon = cloudSun;
+    }
+    return weatherIcon;
+  };
+
+  const forecastDetails = forecast.map((obj, i) => {
+    return (
+      <div className="forecast-day" key={i}>
+        <p className="day">{obj.day}</p>
+        <img
+          src={chooseWeatherIcons()}
+          alt="Cloud Drizzle"
+          width="45"
+          height="47"
+        ></img>
+        <p className="temp">{obj.temp}&#176;</p>
+      </div>
+    );
+  });
   return (
     <>
-      <div className="forecast">
-        <div className="forecast-day">
-          <p className="day">Sun</p>
-          <img
-            src={cloudDrizzle}
-            alt="Cloud Drizzle"
-            width="45"
-            height="47"
-          ></img>
-          <p className="temp">92&#176;</p>
-        </div>
-        <div className="forecast-day">
-          <p className="day">Mon</p>
-          <img
-            src={cloudDrizzleSun}
-            alt="Cloud Drizzle"
-            width="45"
-            height="47"
-          ></img>
-          <p className="temp">92&#176;</p>
-        </div>
-        <div className="forecast-day">
-          <p className="day">Tue</p>
-          <img
-            src={cloudLightning}
-            alt="Cloud Drizzle"
-            width="45"
-            height="47"
-          ></img>
-          <p className="temp">92&#176;</p>
-        </div>
-        <div className="forecast-day">
-          <p className="day">Wed</p>
-          <img src={cloudSun} alt="Cloud Drizzle" width="45" height="47"></img>
-          <p className="temp">92&#176;</p>
-        </div>
-        <div className="forecast-day">
-          <p className="day">Thur</p>
-          <img
-            src={cloudDrizzle}
-            alt="Cloud Drizzle"
-            width="45"
-            height="47"
-          ></img>
-          <p className="temp">92&#176;</p>
-        </div>
-        {/* <div className="forecast-day">
-          <p>Friday</p>
-          <img src={cloudLightning} alt="Cloud Drizzle"></img>
-          <p>92&#176;</p>
-        </div>
-        <div className="forecast-day">
-          <p>Saturday</p>
-          <img src={cloudDrizzleSun} alt="Cloud Drizzle"></img>
-          <p>92&#176;</p>
-        </div> */}
-      </div>
+      <div className="forecast">{forecastDetails}</div>
     </>
   );
 };
